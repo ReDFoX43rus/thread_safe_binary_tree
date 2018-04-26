af309c5e385fde8ea2d6aaf78707ebb314b382c6
ReDFoX43rus <red-fox_ddrace@mail.ru>
2018-04-26 22:40:35 +0300

    required tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "tree.h"
 
-#define THREADS_N 4
-#define REMOVE_VALUES_N 15
-
 using namespace std;
 
 typedef struct {
@@ -11,10 +8,6 @@ typedef struct {
     int to;
     int step;
 } add_info_t;
-
-typedef struct {
-    int values[REMOVE_VALUES_N];
-} remove_info_t;
 
 CTree *tree = new CTree();
 
@@ -28,74 +21,83 @@ void* adder(void* arg){
 }
 
 void *remover(void *arg){
-    remove_info_t *info = (remove_info_t*)arg;
+    add_info_t *info = (add_info_t*)arg;
 
-    for(int i = 0; i < REMOVE_VALUES_N; i++){
-        tree->Remove(info->values[i]);
+    for(int i = info->from; i <= info->to; i += info->step)
+        tree->Remove(i);
+
+    return nullptr;
+}
+
+void* addRandoms(void* arg){
+    add_info_t *info = (add_info_t*)arg;
+
+    int action = rand()%2;
+
+    for(int i = 0; i < info->to; i++){
+        if(action)
+            tree->Add(rand());
+        else tree->Remove(rand());
     }
 
     return nullptr;
 }
 
-int main(){
-    int i = 50;
-    while(i){
-        tree->Add(i);
+void handleAll(int from, int to, bool add){
+    int n = to - from + 1;
 
-        if(i < 0)
-            i += 10;
-        i *= -1;
-    }
+    pthread_t *threads = new pthread_t[n];
+    add_info_t *infos = new add_info_t[n];
 
-    tree->PrintTree();
-
-    tree->Remove(-40);
-    tree->PrintTree();
-    tree->Remove(10);
-    tree->PrintTree();
-
-    for(int i = -50; i <= 50; i += 10){
-        cout << i << " found: " << tree->Find(i) << endl;
-    }
-
-    pthread_t threads[THREADS_N];
-    add_info_t infos[THREADS_N];
-    for(int i = 0; i < THREADS_N; i++){
-        infos[i].from = i*10;
-        infos[i].to = 2*i*10;
+    for(int i = 0; i < n; i++){
+        infos[i].from = i + from;
+        infos[i].to = i + from;
         infos[i].step = 1;
 
-        pthread_create(&threads[i], NULL, adder, (void*)&infos[i]);
+        pthread_create(&threads[i], NULL, add ? adder : remover, (void*)&infos[i]);
     }
 
-    for(int i = 0; i < THREADS_N; i++)
+    for(int i = 0; i < n; i++){
         pthread_join(threads[i], NULL);
-
-    for(int i = 0; i < THREADS_N; i++){
-        infos[i].from = -100 + i*10;
-        infos[i].to = -100 + (i+1)*10;
-        infos[i].step = 2;
-
-        pthread_create(&threads[i], NULL, adder, (void*)&infos[i]);
     }
 
-    for(int i = 0; i < THREADS_N; i++)
-        pthread_join(threads[i], NULL);
 
+    bool all = true, any = false;
+    for(int i = from; i <= to; i++){
+        if(!tree->Find(i)){
+            all = false;
+        } else any = true;
+    }
+
+    cout << "Handle: all " << all << " any: " << any << endl;
+
+    delete[] infos;
+    delete[] threads;
+}
+
+int main(){
+    handleAll(-50, 50, true);
+    tree->PrintTree();
+    handleAll(-50, 50, false);
     tree->PrintTree();
 
-    remove_info_t rm_infos[THREADS_N];
-    for(int i = 0; i < THREADS_N; i++){
-        for(int j = 0; j < REMOVE_VALUES_N; j++)
-            rm_infos[i].values[j] = (i+1)*10 + j;
+    int n = 100;
 
-        pthread_create(&threads[i], NULL, remover, (void*)&rm_infos[i]);
+    pthread_t *threads = new pthread_t[n];
+    add_info_t *infos = new add_info_t[n];
+
+    for(int i = 0; i < n; i++){
+        infos[i].to = 20;
+
+        pthread_create(&threads[i], NULL, addRandoms, (void*)&infos[i]);
     }
 
-    for(int i = 0; i < THREADS_N; i++)
+    for(int i = 0; i < n; i++){
         pthread_join(threads[i], NULL);
+    }
 
-    tree->PrintTree();
+    delete[] infos;
+    delete[] threads;
 
     delete tree;
 
